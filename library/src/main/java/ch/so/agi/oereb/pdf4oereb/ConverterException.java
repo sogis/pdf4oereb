158cf305cc8124bb5ae90fee258036d5c030ed51
Stefan Ziegler <edi.gonzales@gmail.com>
2019-02-28 18:22:16 +0100

    add missing exception class

---
+++ library/src/main/java/ch/so/agi/oereb/pdf4oereb/ConverterException.java
@@ -0,0 +1,7 @@
+package ch.so.agi.oereb.pdf4oereb;
+
+public class ConverterException  extends Exception { 
+    public ConverterException(String errorMessage) {
+        super(errorMessage);
+    }
+} 
